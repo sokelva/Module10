dfa6ed3c39d78575bbd2f3a4dbfc99f51a4fb44e
sokelva <sokelva@mail.ru>
2023-01-19 11:07:24 +0300

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+
+    public interface IManager
+    {
+        void Create();
+        void Read();
+        void Update();
+        void Delete();
+    }
+
+}
